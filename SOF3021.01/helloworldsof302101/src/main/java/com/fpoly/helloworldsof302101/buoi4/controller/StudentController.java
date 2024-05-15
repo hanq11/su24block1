f21175d71f42ae7c1391ae9e3e1c12f48b3a0b0d
Michael <>
2024-05-15 12:42:40 +0700

    update chua lab02 SOF3021.01

--- SOF3021.01/helloworldsof302101/src/main/java/com/fpoly/helloworldsof302101/buoi4/controller/StudentController.java
+++ SOF3021.01/helloworldsof302101/src/main/java/com/fpoly/helloworldsof302101/buoi4/controller/StudentController.java
@@ -1,12 +1,10 @@
 package com.fpoly.helloworldsof302101.buoi4.controller;
 
+import com.fpoly.helloworldsof302101.buoi4.entity.Student;
 import com.fpoly.helloworldsof302101.buoi4.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -21,4 +19,38 @@ public class StudentController {
         return "student/index";
     }
 
+    @GetMapping("/update/{id}")
+    public String showUpdate(@PathVariable("id") String id, Model model) {
+        model.addAttribute("student", service.findById(id));
+        return "student/update";
+    }
+
+    @PostMapping("/update")
+    public String update(Student student) {
+        service.update(student);
+        return "redirect:/student";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") String id) {
+        service.delete(id);
+        return "redirect:/student";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm() {
+        return "student/form";
+    }
+
+    @PostMapping("/create")
+    public String create(Student student) {
+        service.create(student);
+        return "redirect:/student";
+    }
+
+    @GetMapping("/detail")
+    public String showDetailById(@RequestParam("id") String id, Model model) {
+        model.addAttribute("student", service.findById(id));
+        return "student/detail";
+    }
 }
--- SOF3021.01/helloworldsof302101/src/main/webapp/WEB-INF/views/student/update.jsp
+++ SOF3021.01/helloworldsof302101/src/main/webapp/WEB-INF/views/student/update.jsp
@@ -1,21 +1,22 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Sửa thông tin</title>
 </head>
 <body>
 
-<form action="/student/update">
-    MSSV: <input type="text" name="id"  />
+<form action="/student/update" method="post">
+    MSSV: <input type="text" name="id" value="${student.id}" readonly/>
     <br/>
-    Tên: <input type="text" name="name" />
+    Tên: <input type="text" name="name" value="${student.name}"/>
     <br/>
-    Tuổi: <input type="text" name="age" />
+    Tuổi: <input type="text" name="age" value="${student.age}"/>
     <br/>
-    Địa chỉ: <input type="text" name="address" />
+    Địa chỉ: <input type="text" name="address" value="${student.address}"/>
     <br/>
     Giới tính:
-    <input type="radio" name="gender" checked value="true"/>Nam
-    <input type="radio" name="gender" value="false"/>Nữ
+    <input type="radio" name="gender" ${student.gender ? "checked" : ""} value="true"/>Nam
+    <input type="radio" name="gender" ${student.gender ? "" : "checked"} value="false"/>Nữ
     <br/>
     <button>Update</button>
 </form>
